d4515daa7843ccb52c09603807695fce38986827
Jan S. (Milania1) <github@milania.de>
2015-03-26 19:54:54 +0100

    Added test project

---
+++ Testprojects/Testproject_Map/Testproject_Map/Game.cpp
@@ -0,0 +1,47 @@
+#include "StdAfx.h"
+#include "Game.h"
+
+
+
+CGame::CGame(void)
+{
+	// Hier eventuelle Vorinitialisierungen Deiner Vektoria-Objekte einfügen:
+}
+
+CGame::~CGame(void)
+{
+	// Hier eventuelle Nachinitialisierungen Deiner Vektoria-Objekte einfügen:
+}
+
+void CGame::Init(HWND hwnd, CSplash * psplash)
+{
+	// Hier die Initialisierung Deiner Vektoria-Objekte einfügen:
+	m_zr.Init(psplash);
+	m_zf.Init(hwnd);
+	m_zv.InitFull(&m_zc);
+	m_zo.InitFull(&m_zi);
+	m_zi.Init("textures\\red_image.jpg");
+
+	m_zr.AddFrameHere(&m_zf);
+	m_zf.AddViewport(&m_zv);
+	m_zv.AddOverlay(&m_zo);
+}
+
+void CGame::Tick(float fTime, float fTimeDelta)
+{
+	// Hier die Echtzeit-Veränderungen einfügen:
+	m_zr.Tick(fTimeDelta);
+}
+
+void CGame::Fini()
+{
+	// Hier die Finalisierung Deiner Vektoria-Objekte einfügen:
+}
+
+void CGame::WindowReSize(int iNewWidth, int iNewHeight)
+{
+	// Windows ReSize wird immer automatisch aufgerufen, wenn die Fenstergröße verändert wurde.
+	// Hier kannst Du dann die Auflösung des Viewports neu einstellen:
+
+}
+
