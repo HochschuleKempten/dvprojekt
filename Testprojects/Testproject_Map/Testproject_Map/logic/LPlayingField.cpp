158ccb1686743cfdeed8774c797113a0d65c4058
Florian Lutz <florian.lutz86@googlemail.com>
2015-03-28 16:47:26 +0100

    Mergekonflik solved

--- Testprojects/Testproject_Map/Testproject_Map/logic/LPlayingField.cpp
+++ Testprojects/Testproject_Map/Testproject_Map/logic/LPlayingField.cpp
@@ -2,11 +2,7 @@
 
 LPlayingField::LPlayingField(LMaster* lMaster)
 	: lMaster(lMaster),
-<<<<<<< HEAD
-	vField(lMaster->getVMaster()->getFactory()->createPlayingField(lMaster->getVMaster(), this))//Copy des Pointer auf Master in Member
-=======
 	vField(lMaster->getVMaster()->getFactory()->createPlayingField(this))
->>>>>>> b3a8c5bc48b13a8dadb2f24fadfbcb0725634b0e
 {
 	fieldArray = new LField[SIZE*SIZE]; //dynamischer Aufbau des 2D Feldarrays als Aneinanderreihung
 	this->fieldLength = SIZE;
