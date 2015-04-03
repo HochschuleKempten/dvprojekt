d35276c382ac2c7c6b0c37cc8f7e1325b5f125ae
brunobanani <mariobrunnerdvp@gmail.com>
2015-04-03 18:33:46 +0200

    used new factory creation mehtod to get vpowerline in lpowerline constuctor

--- Testprojects/Testproject_Map/Testproject_Map/logic/LPowerLine.cpp
+++ Testprojects/Testproject_Map/Testproject_Map/logic/LPowerLine.cpp
@@ -7,7 +7,7 @@
 
 
 LPowerLine::LPowerLine(const int costs, const int energyValue, LPlayingField* lPlayingField, const int x, const int y)
-:LPowerLine(costs, energyValue, lPlayingField, x, y, lPlayingField->getLMaster()->getVMaster()->getFactory()->createHydroelectricPowerPlant(this))
+:ILPowerLine(costs, energyValue, lPlayingField, x, y, lPlayingField->getLMaster()->getVMaster()->getFactory()->createPowerLine(this))
 {
 	vPowerLine->initPowerLine(x, y);
 }
