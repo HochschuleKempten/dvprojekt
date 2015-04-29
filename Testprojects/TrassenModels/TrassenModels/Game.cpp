419547f46d46a57612eed46c6e50e816688d0abb
Raphael Reich <raphael.reich1337@gmail.com>
2015-04-29 17:30:49 +0200

    Fix merge errors

--- Testprojects/TrassenModels/TrassenModels/Game.cpp
+++ Testprojects/TrassenModels/TrassenModels/Game.cpp
@@ -1,11 +1,8 @@
 #include "StdAfx.h"
 #include "Game.h"
-<<<<<<< HEAD
 #include <filesystem>
 #include "../../../Hochspannung/Hochspannung/view/VMaterialLoader.h"
-=======
 #include "../../../Hochspannung/Hochspannung/view/Helper.h"
->>>>>>> remotes/origin/master
 
 CGame::CGame(void)
 {
