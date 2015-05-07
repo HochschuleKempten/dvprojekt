1221aa6d02a1211b0ef34f5fded2de61e24c99b1
Bastian Pätzold <bastianptzolddvp@gmail.com>
2015-05-07 10:28:48 +0200

    added external balancing file that is read at startup

--- Hochspannung/Hochspannung/logic/LBalanceLoader.cpp
+++ Hochspannung/Hochspannung/logic/LBalanceLoader.cpp
@@ -1,19 +1,22 @@
 #include "LBalanceLoader.h"
+#include <fstream>
 
 NAMESPACE_LOGIC_B
 
-
-DEBUG_EXPRESSION(bool LBalanceLoader::initDone = false);
+boost::property_tree::ptree LBalanceLoader::propertyTree;
 
 void LBalanceLoader::init()
 {
-	DEBUG_EXPRESSION(initDone = true);
+	try {
+		boost::property_tree::read_ini("logic\\Balancing.ini", propertyTree);
+	} catch (boost::property_tree::ptree_error error) {
+		DEBUG_OUTPUT(error.what())
+	}
+
 }
 
 int LBalanceLoader::getFieldStorage(const LField::FieldType fieldType)
 {
-	ASSERT(initDone, "LBalanceLoader is not initialized");
-
 	switch (fieldType) {
 		case LField::CITY: break;
 		case LField::WATER: break;
@@ -32,44 +35,7 @@ int LBalanceLoader::getFieldStorage(const LField::FieldType fieldType)
 
 double LBalanceLoader::getMoneyPerWatt()
 {
-	ASSERT(initDone, "LBalanceLoader is not initialized");
 	return 0.1;
-}
-
-double LBalanceLoader::getMoneyBackAmount()
-{
-	return 0.5;
-}
-
-std::unordered_map<LField::FieldType, double> LBalanceLoader::getFieldTypes()
-{
-	ASSERT(initDone, "LBalanceLoader is not initialized");
-	std::unordered_map<LField::FieldType, double> fieldTypes;
-
-	fieldTypes[LField::SOLAR] = 0.1;
-	fieldTypes[LField::AIR] = 0.1;
-	fieldTypes[LField::WATER] = 0.1;
-	fieldTypes[LField::COAL] = 0.1;
-	fieldTypes[LField::NUCLEAR] = 0.1;
-	fieldTypes[LField::OIL] = 0.1;
-	fieldTypes[LField::MOUNTAIN] = 0.4;
-
-#ifdef _DEBUG
-	double sum = 0.0;
-	for (const auto& pair : fieldTypes) {
-		sum += pair.second;
-	}
-
-	ASSERT(sum == 1.0, "Relations between field types are incorrect");
-#endif //_DEBUG
-
-	return fieldTypes;
-}
-
-std::vector<LField::FieldLevel> LBalanceLoader::getFieldLevels()
-{
-	ASSERT(initDone, "LBalanceLoader is not initialized");
-	return {LField::LEVEL1, LField::LEVEL2, LField::LEVEL3};
 }
 
 NAMESPACE_LOGIC_E
--- Hochspannung/Hochspannung/logic/LBalanceLoader.h
+++ Hochspannung/Hochspannung/logic/LBalanceLoader.h
@@ -2,8 +2,8 @@
 
 #include "LGeneral.h"
 #include "LField.h"
-#include "LUtility.h"
-#include <unordered_map>
+#include <boost\property_tree\ptree.hpp>
+#include <boost\property_tree\ini_parser.hpp>
 
 NAMESPACE_LOGIC_B
 
@@ -25,25 +25,24 @@ private:
 	LBalanceLoader& operator=(const LBalanceLoader&) = delete;
 	LBalanceLoader& operator=(const LBalanceLoader&&) = delete;
 	~LBalanceLoader() = delete;
-	DEBUG_EXPRESSION(static bool initDone);
+
+	static boost::property_tree::ptree propertyTree;
 
 public:
 	static void init();
 
 	template<typename T> static int getCost() { return 0; }
-	template<> static int getCost<LPowerLine>() { return 100; }
-	template<> static int getCost<LWindmillPowerPlant>() { return 500; }
-	template<> static int getCost<LSolarPowerPlant>() { return 500; }
-	template<> static int getCost<LHydroelectricPowerPlant>() { return 500; }
-	template<> static int getCost<LCoalPowerPlant>() { return 500; }
-	template<> static int getCost<LOilRefinery>() { return 500; }
-	template<> static int getCost<LNuclearPowerPlant>() { return 500; }
+	template<> static int getCost<LPowerLine>() { return propertyTree.get<int>("Cost.PowerLine", 0); }
+	template<> static int getCost<LSolarPowerPlant>() { return propertyTree.get<int>("Cost.SolarPowerPlant", 0); }
+	template<> static int getCost<LHydroelectricPowerPlant>() { return propertyTree.get<int>("Cost.HydroelectricPowerPlant", 0); }
+	template<> static int getCost<LCoalPowerPlant>() { return propertyTree.get<int>("Cost.CoalPowerPlant", 0); }
+	template<> static int getCost<LOilRefinery>() { return propertyTree.get<int>("Cost.OilRefinery", 0); }
+	template<> static int getCost<LNuclearPowerPlant>() { return propertyTree.get<int>("Cost.NuclearPowerPlant", 0);}
 
 	static int getFieldStorage(const LField::FieldType fieldType);
+
 	static double getMoneyPerWatt();
-	static double getMoneyBackAmount();
-	static std::unordered_map<LField::FieldType, double> getFieldTypes();
-	static std::vector<LField::FieldLevel> getFieldLevels();
+
 };
 
 
