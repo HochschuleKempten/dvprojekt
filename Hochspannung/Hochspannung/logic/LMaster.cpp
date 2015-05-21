54d9f8e08b34e1b5ad0d99543c1c154bb0ba4792
cortex42 <ivo.palazzolo@gmx.de>
2015-05-21 12:45:18 +0200

    fixed gamelist update bug

--- Hochspannung/Hochspannung/logic/LMaster.cpp
+++ Hochspannung/Hochspannung/logic/LMaster.cpp
@@ -147,8 +147,9 @@ void LMaster::tick(const float fTimeDelta)
 	//	ASSERT(error.what());
 	//}
 
-	if (timeLastCheck > 3.0F && (lPlayingField != nullptr) ? !lPlayingField->isInitDone() : true)
+	if (timeLastCheck > 3.0F && (lPlayingField != nullptr ? !lPlayingField->isInitDone() : true))
 	{
+		DEBUG_OUTPUT("Updated gamelist.");
 		vMaster.updateGameList(getGameList());
 	}
 
