9c092a2c751128a68f77cc575af1945c9a39da4e
brunobanani <mariobrunnerdvp@gmail.com>
2015-06-04 17:36:58 +0200

    removed one line -> Mergeconflict

--- Hochspannung/Hochspannung/view/VCoalPowerPlant.cpp
+++ Hochspannung/Hochspannung/view/VCoalPowerPlant.cpp
@@ -26,8 +26,7 @@ void VCoalPowerPlant::initPowerPlant(const std::shared_ptr<IVPowerPlant>& objPtr
 	viewModel.getMainPlacement()->TranslateZDelta(viewModel.getHeight() * 0.5f * scale - 0.6f);
 
 	vMaster->getVPlayingField()->placeObject(std::dynamic_pointer_cast<IViewBuilding>(objPtr), x, y);
-
-	SET_NAME_AND_COORDINATES(VIdentifier::VCoalPowerPlant); 	
+	
 }
 
 
